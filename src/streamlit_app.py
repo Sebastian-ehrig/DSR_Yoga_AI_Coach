f2c1c1165a677eab7f672a7de3b1cfaed65d2388
sebastian-ehrig <sebast.ehrig@gmail.com>
2022-06-23 20:07:47 +0200

    updated streamlit app and requirements file

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -8,7 +8,8 @@ import glob
 import random
 import tqdm
 import time
-from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
+from streamlit_webrtc import webrtc_streamer
+import av
 
 # for computing cosine similarity frim images
 # from img2vec_pytorch import Img2Vec
@@ -74,21 +75,21 @@ CommandExecuted = False
 # VideoCapture(0) -> webcam
 # VideoCapture(2) -> external cam/webcam
 
-if external_cam==1: # external cam
-    cap = cv2.VideoCapture(2)
-else:
-    cap = cv2.VideoCapture(0) # webcam
+# if external_cam==1: # external cam
+#     cap = cv2.VideoCapture(2)
+# else:
+#     cap = cv2.VideoCapture(0) # webcam
 
-# set camera resolution etc.
-# # 1080p
-# frame_Width = 1920
-# frame_Height= 1080
-# # 720p
-frame_Width = 1280
-frame_Height= 720
+# # set camera resolution etc.
+# # # 1080p
+# # frame_Width = 1920
+# # frame_Height= 1080
+# # # 720p
+# frame_Width = 1280
+# frame_Height= 720
 
-cap.set(3,frame_Width) # Width of the frames in the video stream
-cap.set(4,frame_Height) # Height of the frames in the video stream
+# cap.set(3,frame_Width) # Width of the frames in the video stream
+# cap.set(4,frame_Height) # Height of the frames in the video stream
 
 # # define a white frame
 # white_frame = np.zeros([frame_Height,frame_Width,3],dtype=np.uint8)
@@ -122,166 +123,166 @@ seq_step = 0 # sequence step
 
 # webrtc_streamer(key="example")
 
-class VideoTransformer(VideoTransformerBase):
-    def transform(self, frame):
-        frame = frame.to_ndarray(format="bgr24")
+def video_frame_callback(frame):
 
-        frame = cv2.flip(frame, 1)
-        
-        counter += 1
+    frame = frame.to_ndarray(format="bgr24")
 
-        # input frame has to be a float32 tensor of shape: 256x256x3.
-        # RGB with values in [0, 255].
-        img = frame.copy()   
-        image_height, image_width, _ = frame.shape
+    frame = cv2.flip(frame, 1)
+    
+    counter += 1
 
-        # # make keypoint predictions on image frame
-        # initial_keypoints_with_scores = make_predictions(img, interpreter, image_size)
+    # input frame has to be a float32 tensor of shape: 256x256x3.
+    # RGB with values in [0, 255].
+    img = frame.copy()   
+    image_height, image_width, _ = frame.shape
 
-        # # determine crop region
-        # determine_crop_region(keypoints_with_scores, image_height, image_width)
-        
-        # improve keypoint-predictions by cropping the image around the intitaly detected keypoints
-        crop_region = init_crop_region(image_height, image_width)
-        keypoints_with_scores = improve_predictions(make_predictions_compact, img, crop_region, image_size, interpreter)
+    # # make keypoint predictions on image frame
+    # initial_keypoints_with_scores = make_predictions(img, interpreter, image_size)
 
-        # Render image including the detected keypoints:
-        # ----------------------------------------------
-        
-        confidence_threshold = confidence_score # threshold for drawing the keypoints
+    # # determine crop region
+    # determine_crop_region(keypoints_with_scores, image_height, image_width)
+    
+    # improve keypoint-predictions by cropping the image around the intitaly detected keypoints
+    crop_region = init_crop_region(image_height, image_width)
+    keypoints_with_scores = improve_predictions(make_predictions_compact, img, crop_region, image_size, interpreter)
 
-        # draw the line connections
-        draw_connections(frame, keypoints_with_scores, EDGES, confidence_threshold)
-        
-        # draw the keypoints
-        # draw_keypoints_initial(frame, initial_keypoints_with_scores, confidence_threshold)
-        draw_keypoints(frame, keypoints_with_scores, confidence_threshold)
+    # Render image including the detected keypoints:
+    # ----------------------------------------------
+    
+    confidence_threshold = confidence_score # threshold for drawing the keypoints
 
-        # draw_angles(frame, keypoints_with_scores, confidence_threshold)
+    # draw the line connections
+    draw_connections(frame, keypoints_with_scores, EDGES, confidence_threshold)
+    
+    # draw the keypoints
+    # draw_keypoints_initial(frame, initial_keypoints_with_scores, confidence_threshold)
+    draw_keypoints(frame, keypoints_with_scores, confidence_threshold)
 
-        # run the classifier on the frame
-        prob_list_labels, prob_list_scores, output, labels = classifier(keypoints_with_scores)
+    # draw_angles(frame, keypoints_with_scores, confidence_threshold)
 
-        # Compute cosine-similarity score using the keypoints:
-        # -----------------------------------------------------
-        # get index of closest matching pose
-        pose_idx = np.where(output == np.amax(output))[0][0]
+    # run the classifier on the frame
+    prob_list_labels, prob_list_scores, output, labels = classifier(keypoints_with_scores)
 
-        # current_keypoint_coordinates = keypoints_with_scores[0][0][:,0:2]
-        # current_keypoint_coordinates = np.squeeze(current_keypoint_coordinates)
-        # cos_score = cosine_sim(poses_df[1], current_keypoint_coordinates)
-        # cos_sim_score_kpt = sum(cos_score)/len(cos_score)
+    # Compute cosine-similarity score using the keypoints:
+    # -----------------------------------------------------
+    # get index of closest matching pose
+    pose_idx = np.where(output == np.amax(output))[0][0]
 
-        # cosine similarity from angle differences
-        # ----------------------------------------
-        # cos_sim_score_kpt = cosine_similarity(
-        #     np.array(reference_pose_angles(poses_df[pose_idx])),
-        #     np.array(pose_angles(keypoints_with_scores))
-        #     )
-        keypoints_reference_pose = poses_df[pose_idx]
-        pose_angles_reference_img = np.array(asana_pose_angles_from_reference(keypoints_reference_pose, pose_idx))
-        pose_angles_current_frame = np.array(asana_pose_angles_from_frame(keypoints_with_scores, pose_idx))
+    # current_keypoint_coordinates = keypoints_with_scores[0][0][:,0:2]
+    # current_keypoint_coordinates = np.squeeze(current_keypoint_coordinates)
+    # cos_score = cosine_sim(poses_df[1], current_keypoint_coordinates)
+    # cos_sim_score_kpt = sum(cos_score)/len(cos_score)
 
-        cos_sim_score_kpt = cosine_similarity(
-                                pose_angles_reference_img,
-                                pose_angles_current_frame
-                        )
+    # cosine similarity from angle differences
+    # ----------------------------------------
+    # cos_sim_score_kpt = cosine_similarity(
+    #     np.array(reference_pose_angles(poses_df[pose_idx])),
+    #     np.array(pose_angles(keypoints_with_scores))
+    #     )
+    keypoints_reference_pose = poses_df[pose_idx]
+    pose_angles_reference_img = np.array(asana_pose_angles_from_reference(keypoints_reference_pose, pose_idx))
+    pose_angles_current_frame = np.array(asana_pose_angles_from_frame(keypoints_with_scores, pose_idx))
 
-        mse = (np.square(pose_angles_reference_img - pose_angles_current_frame)).mean()
+    cos_sim_score_kpt = cosine_similarity(
+                            pose_angles_reference_img,
+                            pose_angles_current_frame
+                    )
 
-        # draw class prediction results
-        draw_class_prediction_results(keypoints_with_scores, prob_list_labels, prob_list_scores, frame)
-        # draw cosine-similarity scores
-        draw_cosine_similarity(keypoints_with_scores, cos_sim_score_kpt, mse, frame)
+    mse = (np.square(pose_angles_reference_img - pose_angles_current_frame)).mean()
 
-        # ~https://stackoverflow.com/questions/6893968/how-to-get-the-return-value-from-a-thread-in-python
-        # thread1 = ThreadWithResult(target=yoga_sequence_lead, args=(keypoints_reference_pose, keypoints_with_scores, pose_idx, seq_step, mse))
-        # thread1.start()
-        # thread1.join()
-        # seq_step = thread1.result
-        # if seq_step == 1:
-        #     thread1.sleep(10)
-        if play_yoga_sequence == 1:
-            seq_step, time_in = yoga_sequence_lead(keypoints_reference_pose, keypoints_with_scores, pose_idx, seq_step, mse)
+    # draw class prediction results
+    draw_class_prediction_results(keypoints_with_scores, prob_list_labels, prob_list_scores, frame)
+    # draw cosine-similarity scores
+    draw_cosine_similarity(keypoints_with_scores, cos_sim_score_kpt, mse, frame)
 
-        # # --------------------------------
-        # # make pose-correction suggestions
-        # # --------------------------------
+    # ~https://stackoverflow.com/questions/6893968/how-to-get-the-return-value-from-a-thread-in-python
+    # thread1 = ThreadWithResult(target=yoga_sequence_lead, args=(keypoints_reference_pose, keypoints_with_scores, pose_idx, seq_step, mse))
+    # thread1.start()
+    # thread1.join()
+    # seq_step = thread1.result
+    # if seq_step == 1:
+    #     thread1.sleep(10)
+    if play_yoga_sequence == 1:
+        seq_step, time_in = yoga_sequence_lead(keypoints_reference_pose, keypoints_with_scores, pose_idx, seq_step, mse)
 
-        if play_yoga_sequence == 1: # corrections for yoga sequence
+    # # --------------------------------
+    # # make pose-correction suggestions
+    # # --------------------------------
 
-            if seq_step == 0 or seq_step >=5:
+    if play_yoga_sequence == 1: # corrections for yoga sequence
+
+        if seq_step == 0 or seq_step >=5:
+            if counter % 50 == 0: # suggest corrections every 50 frames (~ 2 seconds)
+                if mse <= 200:
+                    correct = True
+                if mse > 201:              
+                    correct_angles(keypoints_reference_pose, keypoints_with_scores, pose_idx)
+
+
+        if seq_step == 1:
+            if time.time() - time_in > 90:
                 if counter % 50 == 0: # suggest corrections every 50 frames (~ 2 seconds)
                     if mse <= 200:
                         correct = True
                     if mse > 201:              
                         correct_angles(keypoints_reference_pose, keypoints_with_scores, pose_idx)
 
-
-            if seq_step == 1:
-                if time.time() - time_in > 90:
-                    if counter % 50 == 0: # suggest corrections every 50 frames (~ 2 seconds)
-                        if mse <= 200:
-                            correct = True
-                        if mse > 201:              
-                            correct_angles(keypoints_reference_pose, keypoints_with_scores, pose_idx)
-
-            if seq_step == 2:
-                if time.time() - time_in > 50:
-                    if counter % 50 == 0: # suggest corrections every 50 frames (~ 2 seconds)
-                        if mse <= 200:
-                            correct = True
-                        if mse > 201:              
-                            correct_angles(keypoints_reference_pose, keypoints_with_scores, pose_idx)
-
-            if seq_step == 3:
-                if time.time() - time_in > 45:
-                    if counter % 50 == 0: # suggest corrections every 50 frames (~ 2 seconds)
-                        if mse <= 200:
-                            correct = True
-                        if mse > 201:              
-                            correct_angles(keypoints_reference_pose, keypoints_with_scores, pose_idx)
-
-            if seq_step == 4:
-                if time.time() - time_in > 95:
-                    if counter % 50 == 0: # suggest corrections every 50 frames (~ 2 seconds)
-                        if mse <= 200:
-                            correct = True
-                        if mse > 201:              
-                            correct_angles(keypoints_reference_pose, keypoints_with_scores, pose_idx)
-
-        else: # corrections for all poses
-            if counter % 50 == 0: # suggest corrections every 50 frames (~ 2 seconds)
-                    if mse <= 150:
+        if seq_step == 2:
+            if time.time() - time_in > 50:
+                if counter % 50 == 0: # suggest corrections every 50 frames (~ 2 seconds)
+                    if mse <= 200:
                         correct = True
-                        CorrectPose = "./src/functions/sequence_commands/Correct.ogg"
-                        playSound(CorrectPose)
                     if mse > 201:              
                         correct_angles(keypoints_reference_pose, keypoints_with_scores, pose_idx)
-            
 
-        # if counter % 50 == 0: # suggest corrections every 50 frames (~ 2 seconds)
-        #     if mse <= 200:
-        #         correct = True
-        #     if mse > 201:              
-        #         correct_angles(keypoints_reference_pose, keypoints_with_scores, pose_idx)                
+        if seq_step == 3:
+            if time.time() - time_in > 45:
+                if counter % 50 == 0: # suggest corrections every 50 frames (~ 2 seconds)
+                    if mse <= 200:
+                        correct = True
+                    if mse > 201:              
+                        correct_angles(keypoints_reference_pose, keypoints_with_scores, pose_idx)
 
+        if seq_step == 4:
+            if time.time() - time_in > 95:
+                if counter % 50 == 0: # suggest corrections every 50 frames (~ 2 seconds)
+                    if mse <= 200:
+                        correct = True
+                    if mse > 201:              
+                        correct_angles(keypoints_reference_pose, keypoints_with_scores, pose_idx)
 
-        # draw_FPS(frame, counter, fps, start_time) 
+    else: # corrections for all poses
+        if counter % 50 == 0: # suggest corrections every 50 frames (~ 2 seconds)
+                if mse <= 150:
+                    correct = True
+                    CorrectPose = "./src/functions/sequence_commands/Correct.ogg"
+                    playSound(CorrectPose)
+                if mse > 201:              
+                    correct_angles(keypoints_reference_pose, keypoints_with_scores, pose_idx)
         
-        cv2.imshow('MoveNet frame', frame)
 
-        if capture_frames == 1:
-            cv2.imwrite('./frames/Frame'+str(counter)+'.jpg', frame)
-        
-        # define brake-out: if we hit "q" on the keyboard
-        # frame capure is stopped
-        # if cv2.waitKey(10) & 0xFF==ord('q'):
-        #     break
+    # if counter % 50 == 0: # suggest corrections every 50 frames (~ 2 seconds)
+    #     if mse <= 200:
+    #         correct = True
+    #     if mse > 201:              
+    #         correct_angles(keypoints_reference_pose, keypoints_with_scores, pose_idx)                
+
+
+    # draw_FPS(frame, counter, fps, start_time) 
     
-        return frame
+    # cv2.imshow('MoveNet frame', frame)
 
-webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)   
+    # if capture_frames == 1:
+    #     cv2.imwrite('./frames/Frame'+str(counter)+'.jpg', frame)
+    
+    # define brake-out: if we hit "q" on the keyboard
+    # frame capure is stopped
+    # if cv2.waitKey(10) & 0xFF==ord('q'):
+    #     break
+
+    return av.VideoFrame.from_ndarray(frame, format="bgr24")
+
+webrtc_streamer(key="example", video_frame_callback=video_frame_callback)   
 # cap.release() # release the camera
 
 # cv2.destroyAllWindows() # close all windows
